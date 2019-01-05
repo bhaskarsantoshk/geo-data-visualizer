146b0dc970c258dd49a7c7ef3d9e0cf7e8f2ca50
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-01-05 21:32:43 +0530

    maps

--- src/module6/EarthquakeMarker.java
+++ src/module6/EarthquakeMarker.java
@@ -3,7 +3,6 @@ package module6;
 import de.fhpotsdam.unfolding.data.PointFeature;
 import processing.core.PConstants;
 import processing.core.PGraphics;
-
 /** Implements a visual marker for earthquakes on an earthquake map
  * 
  * @author UC San Diego Intermediate Software Development MOOC team
